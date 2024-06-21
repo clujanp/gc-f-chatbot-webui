a294cdbf027b81f76b6f7a38a7cf49e65bf02011
jfarfa3 <jfarfa3@bancodebogota.com.co>
2024-06-21 15:57:45 -0500

    chore: Update Vite configuration for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/gc-f-chatbot-webui/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]',
+      },
+    },
+  }
 });
