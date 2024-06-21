5c70f727f397ae3884232bb45d73c1dbca47fbe9
jfarfa3 <jfarfa3@bancodebogota.com.co>
2024-06-21 10:19:00 -0500

    fix tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,8 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <div className="bg-red-500 border">
+              <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
