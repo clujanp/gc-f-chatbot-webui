9ec34d2cefc2aadafbe47644cf25c9ccd50b1058
jfarfa3 <jfarfa3@bancodebogota.com.co>
2024-06-21 15:16:46 -0500

    feat: Add variant and fit props to Button component

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,14 +2,21 @@ export type ButtonProps = {
   label: string;
   onClick?: () => void;
   type: 'button' | 'submit' | 'reset';
+	variant: 'primary' | 'secondary' | 'danger' | 'success';
+	fit: boolean;
 };
 
-export default function Button({ label, onClick, type }: ButtonProps) {
+export default function Button({ label, onClick, type, variant, fit }: ButtonProps) {
   return (
     <button
       onClick={onClick}
       type={type}
       className='p-2 text-white bg-blue-500 border border-gray-300 rounded-md'
+			style={{
+				backgroundColor: variant === 'primary' ? 'rgb(96 165 250)' : variant === 'danger' ? 'rgb(248 113 113)' : variant === 'success' ? 'rgb(16 185 129)' : 'transparent',
+				color: variant === 'secondary' ? 'rgb(96 165 250)' : 'white',
+				width: fit ? 'fit-content' : 'auto',
+			}}
     >
       {label}
     </button>
